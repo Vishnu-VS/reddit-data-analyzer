a2a8ddce4bc4d21f17dd0e1e57a5b49599787af4
Vishnu V S <vishnu.vs@yahoo.com>
2020-12-29 18:29:53 +0530

    remove light theme options, add primeflex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,25 +9,12 @@ import { DOCUMENT } from '@angular/common';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private primengConfig: PrimeNGConfig, @Inject(DOCUMENT) private document){}
+  constructor(private primengConfig: PrimeNGConfig){}
 
   title = 'Reddit Data Analyzer';
-  stateOptions: any[];
-  value1: string = "dark";
 
   ngOnInit() {
-    this.stateOptions = [{label: 'Dark', value: 'dark'}, {label: 'Light', value: 'light'}];
     this.primengConfig.ripple = true;
-  }
-
-  switchTheme(e){
-    // console.log(e);
-    if(e.value == "light"){
-      this.document.getElementById('theme').setAttribute('href', 'assets/themes/saga-blue/theme.css');
-    }
-    else{
-      this.document.getElementById('theme').setAttribute('href', 'assets/themes/vela-blue/theme.css');
-    }
   }
 
 }
